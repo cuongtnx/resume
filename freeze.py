0e5867498bee9f893edaad65d919271233fd6355
Cuong Tran <cuongtran92@gmail.com>
2017-10-12 11:00:47 -0400

    modified skills

--- freeze.py
+++ freeze.py
@@ -1,10 +1,11 @@
 from flask.ext.frozen import Freezer
 from resume import app
 
+
 freezer = Freezer(
     app=app,
     with_static_files=True
-    )
+)
 
 if __name__ == '__main__':
     freezer.freeze()
